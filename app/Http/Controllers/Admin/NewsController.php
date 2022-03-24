f5c0afc14913c72ec2e78a01556d66086a79dc15
Hara <ecohumankaisei@gmail.com>
2022-03-24 04:04:09 +0000

    laravel08 task completed

---
+++ app/Http/Controllers/Admin/NewsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    //
+    public function add()
+    {
+        return view('admin.news.create');
+    }
+    
+}
