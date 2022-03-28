ad0e6f03579cc569ff961a5826aa729ed926f2d4
Hara <ecohumankaisei@gmail.com>
2022-03-28 15:54:22 +0000

    laravel12 task completed

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');  
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 /*
@@ -31,3 +31,7 @@ Route::group(['prefix' => 'XXX'], function() {
 ?>
 
 */
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
