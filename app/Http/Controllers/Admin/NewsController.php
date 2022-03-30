9652681401ed08e3f76d93968db5f57aac196d8a
Hara <ecohumankaisei@gmail.com>
2022-03-30 05:34:01 +0000

    laravel13 task completed

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -12,5 +12,9 @@ class NewsController extends Controller
     {
         return view('admin.news.create');
     }
-    
+    public function create(Request $request)
+    {
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
+    }  
 }
