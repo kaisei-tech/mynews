ffc54fc152ec28d53c9581acfe23bece8a7e87ff
Hara <ecohumankaisei@gmail.com>
2022-04-08 03:13:13 +0000

    composer.jsonとcomposer.lockをherokuデプロイ用に最適化しました

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (\App::environment('production')) {
+        \URL::forceScheme('https');
+    }
+    
     }
 }
